546f245e85e66865735d059dce2c0ad7f30ce499
Jake Lee <jake.jklee425@gmail.com>
2020-07-16 06:09:11 -0700

    _

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = ({ location }) => {
+  return (
+    <>
+      <h1>404_Not Found.</h1>
+      <p>Oops! The page you are looking for has been removed or relocated.</p>
+    </>
+  )
+}
+
+export default NotFound
