ad752255bb501eccccf8810821fd998c4293c0c1
Jake Lee <jake.jklee425@gmail.com>
2020-07-16 19:47:47 -0700

    added animation

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import { useSpring, animated } from 'react-spring'
 import Img from 'gatsby-image'
 import styled from 'styled-components'
 import IconLink from './iconLink'
-
-const Container = styled.div`
+const CardContainer = styled.div`
   display: flex;
   position: relative;
   justify-self: center;
@@ -13,81 +13,84 @@ const Container = styled.div`
   height: 80%;
   max-width: 340px;
   max-height: 480px;
+  box-shadow: 1px 1px 3px grey;
+  border-radius: 5px;
 `
 const Background = styled(Img)`
   position: absolute;
   width: 100%;
   height: 100%;
-  z-index: -1;
+  border-radius: 5px;
   & > img {
-    object-fit: cover !important;
+    object-fit: fit !important;
     object-position: 0% 0% !important;
   }
 `
-const DescriptionBox = styled.div`
+const DescriptionBox = styled(animated.div)`
   display: flex;
   flex-direction: column;
   position: absolute;
   height: 100%;
   width: 100%;
-  justify-content: flex-end;
-  padding: 10px;
-  background: linear-gradient(
-    220deg,
-    rgba(255, 255, 255, 0.5),
-    rgba(115, 114, 114, 0.5)
-  );
-  border-radius: 15px;
-  box-shadow: 2px 2px 10px lightgrey;
-`
-const Title = styled.div`
-  display: flex;
-  color: White;
-  height: 10%;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-`
-const Button = styled.button`
-  border: 1px solid;
   border-radius: 5px;
-  padding: 3px;
-  height: 1.5rem;
-  background: white;
+`
+const Title = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 0;
 `
 const Links = styled.div`
   display: flex;
-  align-items: center;
-  text-align: center;
-  height: 1.5rem;
-  font-size: 1.5rem;
-
-  a {
-    padding: 3px;
-    height: 1.5rem;
-    font-size: 1.2rem;
-    color: white;
-    border: ${props => props.giveborder === 'true' && '1px solid'};
-  }
+  width: 25%;
+  justify-content: space-around;
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin: 10px;
 `
-const ProjectCard = ({ data: { title, url, git, image, library } }) => {
+const ProjectCard = ({ data: { title, url, git, image } }) => {
+  const [color, setColor] = useSpring(() => ({
+    opacity: 0,
+    color: 'black',
+    config: { duration: 700 }
+  }))
+  const [background, setBackground] = useSpring(() => ({
+    background:
+      'linear-gradient(220deg, rgba(207, 207, 207, 0), rgba(170, 172, 174, 0.8))'
+  }))
   return (
-    <Container>
+    <CardContainer>
       <Background fluid={image} />
-      <DescriptionBox>
-        <Title>
-          <h2>{title}</h2>
-          <Button disabled>{library}</Button>
-        </Title>
+      <DescriptionBox
+        style={{ ...color, ...background }}
+        onMouseEnter={() => {
+          setColor({
+            opacity: 1,
+            color: 'white'
+          })
+          setBackground({
+            background:
+              'linear-gradient(220deg, rgba(207, 207, 207, 0.5), rgba(30, 43, 49, 1))'
+          })
+        }}
+        onMouseLeave={() => {
+          setColor({
+            opacity: 0,
+            color: 'black'
+          })
+          setBackground({
+            background:
+              'linear-gradient(220deg, rgba(207, 207, 207, 0), rgba(170, 172, 174, 0.8))'
+          })
+        }}
+      >
+        <Title>{title}</Title>
         <Links>
-          <IconLink git url={git} style={{ marginRight: '5px' }} />
-          {url ? (
-            <a href={url} giveborder='true'>
-              LIVE
-            </a>
-          ) : null}
+          <IconLink git url={git} />
+          {url ? <a href={url}>LIVE</a> : null}
         </Links>
       </DescriptionBox>
-    </Container>
+    </CardContainer>
   )
 }
 
