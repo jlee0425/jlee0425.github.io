2dae27056f5a79cdca42b2ec25e6f654e6badf2a
Jake Lee <jake.jklee425@gmail.com>
2020-08-13 11:33:29 -0700

    prismic initial commit

--- src/templates/detail.jsx
+++ src/templates/detail.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import styled from 'styled-components'
+
+import { Head } from '../components/slices'
+import SliceZone from '../components/SliceZone'
+
+const Container = styled.div`
+  height: 100%;
+  padding: 7vh 5vh 3vh 5vh;
+`
 
 const ProjectDetail = ({ data: { prismicProjectDetail } }) => {
   const { data } = prismicProjectDetail
+  const headerData = {
+    ...data.title.text,
+    ...data.date.text,
+    ...data.summary.text,
+    ...data.git.url,
+    ...data.link.url
+  }
   return (
-    <>
-      <h1>{data.title.text}</h1>
-    </>
+    <Container>
+      <Head data={headerData} />
+      <SliceZone slices={data.body1} />
+    </Container>
   )
 }
 
