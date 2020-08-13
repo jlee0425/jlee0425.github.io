5fc6f6294af529f0320daf99f6a1e98427f9ea98
Jake Lee <jake.jklee425@gmail.com>
2020-08-13 07:46:28 -0700

    added prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 module.exports = {
   siteMetadata: {
     title: `Jake Lee`,
@@ -38,6 +41,18 @@ module.exports = {
             variants: [`400`, `700`]
           }
         ]
+      }
+    },
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: 'jlee0425',
+        accessToken: `${process.env.API_KEY}`,
+        linkResolver: ({ node, key, value }) => post => `/projects/${post.uid}`,
+        schemas: {
+          project_detail: require('./src/schemas/project_detail.json')
+        },
+        shouldDownloadImage: ({ node, key, value }) => true
       }
     }
   ]
