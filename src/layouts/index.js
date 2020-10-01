02050db001caf3af9c7e4cf80b0f23dded742720
Jake Lee <jake.jklee425@gmail.com>
2020-09-30 19:09:23 -0700

    updated layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { TransitionProvider, TransitionViews } from 'gatsby-plugin-transitions'
-import PropTypes from 'prop-types'
-import { createGlobalStyle } from 'styled-components'
+import React from "react"
+import { TransitionProvider, TransitionViews } from "gatsby-plugin-transitions"
+import PropTypes from "prop-types"
+import { createGlobalStyle } from "styled-components"
 
-import Header from '../components/header'
+import Header from "../components/Header"
 
 const Global = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    height: 100%;
-    width: 100%;
+    height: 100vh;
+    width: 100vw;
     font-family: 'Roboto';
     color: #29323c;
     text-rendering: optimizeLegibility;
@@ -22,39 +22,44 @@ const Global = createGlobalStyle`
     }
     a:hover {
       transform: scale(1.1);
-    }        
+    }   
+    > * {
+      box-sizing: border-box;
+    }     
   }
 `
 const Layout = ({ children, location }) => {
   return (
     <TransitionProvider
       location={location}
-      mode='immediate'
+      mode="immediate"
       leave={{
         opacity: 0,
-        transform: 'translate3d(0, 0%, 0) scale3d(0.5, 0.5, 1)',
+        transform: "translate3d(0, 0%, 0) scale3d(0.5, 0.5, 1)",
         config: {
-          duration: 500
-        }
+          duration: 500,
+        },
       }}
       enter={{
         opacity: 0,
-        transform: 'translate3d(0, 20%, 0) scale3d(1, 1, 1)'
+        transform: "translate3d(0, 20%, 0) scale3d(1, 1, 1)",
       }}
       usual={{
         opacity: 1,
-        transform: 'translate3d(0, 0%, 0) scale3d(1, 1, 1)'
+        transform: "translate3d(0, 0%, 0) scale3d(1, 1, 1)",
       }}
     >
       <Global />
       <Header />
-      <TransitionViews>{children}</TransitionViews>
+      <TransitionViews style={{ paddingTop: "75px" }}>
+        {children}
+      </TransitionViews>
     </TransitionProvider>
   )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 }
 
 export default Layout
