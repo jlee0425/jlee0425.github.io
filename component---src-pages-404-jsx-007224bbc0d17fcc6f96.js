992026a39b537c5d9de687040cb1499167cc1dd2
Jake Lee <jake.jklee425@gmail.com>
2020-09-30 19:09:49 -0700

    Updates

--- component---src-pages-404-jsx-007224bbc0d17fcc6f96.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{pssB:function(e,n,o){"use strict";o.r(n);var a=o("q1tI"),t=o.n(a),l=o("vOnD").b.div.withConfig({displayName:"sc-404__Main",componentId:"sc-1e90w3g-0"})(["padding:8vh;"]);n.default=function(e){e.location;return t.a.createElement(l,null,t.a.createElement("h1",null,"404_Not Found."),t.a.createElement("p",null,"Oops! The page you are looking for has been removed or relocated."))}}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-007224bbc0d17fcc6f96.js.map
